157e9650d0c219f9b3fcec111a4d632a30461bd2
NarasimhaReddy214 <narasimhareddyg214@gmail.com>
2023-04-27 19:02:10 +0530

    Name change

--- src/main/java/com/HostelMS/Controller/HostelController.java
+++ src/main/java/com/HostelMS/Controller/HostelController.java
@@ -18,7 +18,7 @@ public class HostelController {
     @Autowired
     private HostelRepository hostelRepository;
 
-    @GetMapping("/hostels")
+    @GetMapping("/hostelsed")
     public List<Hostels> getAllDetails() {
         return hostelService.getAllDetails();
     }
